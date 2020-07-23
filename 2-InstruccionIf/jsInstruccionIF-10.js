94e0905f29673a85d6e7b596a8577b260b68762c
FranDallasta <dallastafranco@gmail.com>
2020-07-23 20:00:10 -0300

    actualizado

--- 2-InstruccionIf/jsInstruccionIF-10.js
+++ 2-InstruccionIf/jsInstruccionIF-10.js
@@ -1,6 +1,16 @@
 function mostrar()
 {
-	//Genero el número RANDOM entre 1 y 10 
-	alert("ok");
-
+	var nota = Math.floor(Math.random()* 10);
+	if ((nota <= 10) && (nota>=9))
+	{
+		alert("EXCELENTE");
+	}
+	else if((nota < 9) && (nota >= 4))
+	{
+		alert("APROBÓ");
+	}
+	else
+	{
+		alert("Vamos, la proxima se puede");
+	}
 }//FIN DE LA FUNCIÓN
