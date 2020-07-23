fe8f7eb6ae397ff87103acc9bafee52b57e6f47e
FranDallasta <dallastafranco@gmail.com>
2020-07-23 19:49:39 -0300

    actualizado el 23/07

--- 2-InstruccionIf/jsInstruccionIF-06.js
+++ 2-InstruccionIf/jsInstruccionIF-06.js
@@ -1,8 +1,17 @@
 function mostrar()
 {
-	//tomo la edad  
-	alert("ok");
-
-
+	var edad = document.getElementById("txtIdEdad").value;
+	if (( edad >= 13)&&(edad <= 17))
+	{
+		alert("Es adolescente");
+	}
+	else if(edad>17)
+	{
+		alert("Es adulto");
+	}
+	else
+	{
+		alert("Es un niño");
+	}
 
 }//FIN DE LA FUNCIÓN
--- 2-InstruccionIf/jsInstruccionIF-08.js
+++ 2-InstruccionIf/jsInstruccionIF-08.js
@@ -1,8 +1,9 @@
 function mostrar()
 {
-	//tomo la edad  
-	alert("ok");
-	
-
-
-}//FIN DE LA FUNCIÓN
+	var edad = document.getElementById("txtIdEdad").value;
+	var estadoCivil = document.getElementById("estadoCivil").value;
+	if ((edad > 18) && (estadoCivil= "Soltero"))
+	{
+		alert("Es soltero y no es menor");
+	}
+}
